5f83831e6d3904b2bd3647af41b21adb426de219
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-08-18 15:50:59 +0200

    Set dock delay

--- custom-Settings.sh
+++ custom-Settings.sh
@@ -3,3 +3,9 @@ defaults write com.apple.screencapture type jpg
 defaults write com.apple.screencapture disable-shadow true
 
 killall SystemUIServer
+
+# Set dock delay
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0.4
+killall Dock
--- restore-Settings.sh
+++ restore-Settings.sh
@@ -3,3 +3,9 @@ defaults write com.apple.screencapture type png
 defaults write com.apple.screencapture disable-shadow false
 
 killall SystemUIServer
+
+# Restore dock delay
+defaults delete com.apple.dock autohide
+defaults delete com.apple.dock autohide-delay
+defaults delete com.apple.dock autohide-time-modifier
+killall Dock
