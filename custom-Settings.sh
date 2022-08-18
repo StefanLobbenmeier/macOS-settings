b3718e31d46324659aeb2a57dcef5e52720a02d5
Stefan Lobbenmeier <stefan.lobbenmeier@objego.de>
2022-08-18 15:39:02 +0200

    Add screenshot settings

---
+++ custom-Settings.sh
@@ -0,0 +1,5 @@
+# Set screenshots: jpg and no shadow
+defaults write com.apple.screencapture type jpg
+defaults write com.apple.screencapture disable-shadow true
+
+killall SystemUIServer
---
+++ restore-Settings.sh
@@ -0,0 +1,5 @@
+# Set screenshots: png and shadow
+defaults write com.apple.screencapture type png
+defaults write com.apple.screencapture disable-shadow false
+
+killall SystemUIServer
